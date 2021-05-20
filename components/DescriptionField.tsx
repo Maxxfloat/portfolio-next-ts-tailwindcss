c36296d68f758b364de83710c9640586f4b9ea7d
Omid-Neshati <65368035+Maxxfloat@users.noreply.github.com>
2021-05-20 07:26:21 +0430

    description button separate from field sides and styled

--- components/DescriptionField.tsx
+++ components/DescriptionField.tsx
@@ -9,28 +9,57 @@ interface cardContentType {
   style: object;
 }
 
-const DescriptionField: React.FC<cardContentType> = ({
-  front,
-  back,
-  className,
-  style,
-}) => {
-  const [cardContent, setCardContent] = useState("bio");
+const DescriptionField: React.FC<cardContentType> = ({ className, style }) => {
   const [flip, setFlip] = useState(false);
 
-  const side = (style, content: string) => (
-    <div
-      className={`${style} bg-gray-100 rounded-3xl bg-opacity-100 absolute w-full h-full p-5 box-border`}
-    >
-      <div className="h-2/3 block">{content}</div>
-      <div className="flex space-x-3 p-3 box-border justify-center h-1/3">
-        <button onClick={() => setFlip(true)} className="border-black border-2">
-          مهارت ها
-        </button>
-        <button onClick={() => setFlip(false)}>مشخصات من</button>
+  const side = (style: string, content: string) => {
+    // const sideBtns = [
+    //   {
+    //     title: "مهارت ها",
+    //     clickFnc: () => setFlip(true),
+    //   },
+    //   {
+    //     title: "مشخصات",
+    //     clickFnc: () => setFlip(false),
+    //   },
+    // ];
+
+    return (
+      <div
+        className={`${style} bg-gray-100 rounded-t-3xl bg-opacity-100 absolute w-full h-full p-5 box-border`}
+        style={{ boxShadow: "0 4px -1px -1px rgba(0, 0, 0, 0.1)" }}
+      >
+        <div className="h-3/4 block">{content}</div>
+        <div className="flex justify-around space-x-2 items-center p-3 box-border h-1/4">
+          {/* {sideBtns.map((btn, index) => (
+            <button
+              key={index}
+              className={`px-2 h-14 w-full border-2 bg-red-400 border-red-800 rounded-md`}
+              onClick={btn.clickFnc}
+            >
+              {btn.title}
+            </button>
+          ))} */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  const sideBtns = [
+    {
+      title: "مهارت ها",
+      bg: flip ? "bg-red-800 text-white" : "bg-transparent",
+      btnStyle: "border-l-0 rounded-r-sm",
+      clickFnc: () => setFlip(true),
+    },
+    {
+      title: "مشخصات",
+      bg: !flip ? "bg-red-800 text-white" : "bg-transparent",
+      btnStyle: "border-r-0 rounded-l-sm",
+      clickFnc: () => setFlip(false),
+    },
+  ];
+
   return (
     <>
       <animated.div
@@ -41,10 +70,22 @@ const DescriptionField: React.FC<cardContentType> = ({
           className={`
             ${s.inner} 
             ${flip ? s.flip : ""}
-            box-border shadow-md rounded-3xl relative w-full h-full `}
+            box-border relative w-full h-4/5 `}
         >
           {side(s.front, "front")}
           {side(s.back, "back")}
+        </div>
+        <div className="flex justify-around space-x-1 items-center box-border rounded-b-3xl w-full h-auto bg-gray-100 bg-opacity-100 overflow-hidden shadow-md">
+          {sideBtns.map((btn, index) => (
+            <button
+              key={index}
+              className={`${btn.bg} ${btn.btnStyle} px-2 h-14 w-full border-2 border-b-0 border-red-900 active:bg-red-200 `}
+              onClick={btn.clickFnc}
+              style={{ outline: "none" }}
+            >
+              {btn.title}
+            </button>
+          ))}
         </div>
       </animated.div>
     </>
